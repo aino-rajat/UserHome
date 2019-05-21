899dc73131ace019482b0e24957af9fcdee37fda
intern3 <intern3@intern3>
2019-05-21 19:25:39 +0530

    updated code for user home page completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserHomeComponent } from './user-home/user-home.component';
-
+import {MatButtonModule} from '@angular/material/button';
 
 
 
@@ -16,6 +17,8 @@ import { UserHomeComponent } from './user-home/user-home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
    
 
   ],
